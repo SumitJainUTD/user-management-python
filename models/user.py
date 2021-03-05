5bf9ff3e1c30775052b294948f43363757563f0f
sumitjain <sumi12june@gmail.com>
2021-03-05 12:46:16 -0600

    sqllite + find functions update

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 import datetime
+import sqlite3
 
 
 class UserModel:
@@ -12,32 +13,66 @@ class UserModel:
         self.email = email
         self.created_at = datetime.datetime.utcnow().strftime("%d-%b-%Y (%H:%M:%S.%f)")
 
+    def __init__(self, _id, user_name, password, email, created_at):
+        self._id = _id
+        self.user_name = user_name
+        self.password = password
+        self.email = email
+        self.created_at = created_at
+
     def json(self):
         return {
-                '_id': self._id,
-                'user_name': self.user_name,
-                'email': self.email,
-                'created_at': self.created_at
-            }
+            '_id': self._id,
+            'user_name': self.user_name,
+            'email': self.email,
+            'created_at': self.created_at
+        }
+
+    @classmethod
+    def get_user_obj(cls, user_name, password, email):
+        cls.ids += 1
+        return UserModel(cls.ids, user_name, password, email)
+        UserModel.users.append(user)
+        return user
 
     @classmethod
     def add_user(cls, user_name, password, email):
         cls.ids += 1
-        return UserModel(cls.ids, user_name, password, email)
+        user = UserModel(cls.ids, user_name, password, email)
+        UserModel.users.append(user)
+        return user
 
     @classmethod
     def find_by_user_name(cls, name):
-        for u in cls.users:
-            if u.user_name == name:
-                return u
-        return None
+        connection = sqlite3.Connection('database.db')
+        cursor = connection.cursor();
+
+        query = "SELECT * FROM users where user_name=?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        if row:
+            user = UserModel(row[0], row[1], row[2], row[3], row[4])
+        else:
+            user = None
+
+        connection.close()
+        return user
 
     @classmethod
     def find_by_user_id(cls, _id):
-        for u in cls.users:
-            if str(u._id) == str(_id):
-                return u
-        return None
+        connection = sqlite3.Connection('database.db')
+        cursor = connection.cursor();
+
+        query = "SELECT * FROM users where _id=?"
+        result = cursor.execute(query, (_id,))
+        row = result.fetchone()
+        if row:
+            user = UserModel(row[0], row[1], row[2], row[3], row[4])
+        else:
+            user = None
+
+        connection.close()
+        return user
 
     @classmethod
     def delete_user_id(cls, _id):
@@ -49,7 +84,16 @@ class UserModel:
 
     @classmethod
     def find_by_email_id(cls, email):
-        for u in cls.users:
-            if u.email == email:
-                return u
-        return None
+        connection = sqlite3.Connection('database.db')
+        cursor = connection.cursor();
+
+        query = "SELECT * FROM users where email=?"
+        result = cursor.execute(query, (email,))
+        row = result.fetchone()
+        if row:
+            user = UserModel(row[0], row[1], row[2], row[3], row[4])
+        else:
+            user = None
+
+        connection.close()
+        return user
